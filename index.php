ca36424d8720cd85d624106dd3008288fb47648f
brainfoolong <brainfoolong@users.noreply.github.com>
2022-02-14 17:37:58 +0100

    initial

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-const FRAMELIX_MODULE = "Myself";
+const FRAMELIX_MODULE = "Demo";
 define("FRAMELIX_ENTRY_POINT_FOLDER", str_replace("\\", "/", __DIR__));
 require __DIR__ . "/modules/Framelix/public/index.php";
